dc1338f3c6690ce6b9f8d8dd817eaebbb2eaeb8d
MD AZIZUL HAKIM <ahslucky12@gmail.com>
2017-06-30 18:31:44 +0600

    Initial commit

---
+++ JavaScript/HelloWorld.js
@@ -0,0 +1,13 @@
+/**
+ * Created by MD AZIZUL HAKIM on 05/06/2017.
+ */
+
+
+// console.log("Hello World!");
+// var s = "Lol";
+// var x = "Double Lol";
+// console.log(s + " " + x);
+
+for(var i=0; i<5; i++){
+    alert("Hello world")
+}
